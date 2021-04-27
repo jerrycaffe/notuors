83d287a73c8f2240f99d924fe7b9abdceeb27d30
jerrycaffe@gmail.com <jerrycaffe@gmail.com>
2021-04-27 20:10:07 +0100

    commited

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -54,18 +54,34 @@ exports.postTour = (req, res) => {
   //   }
   // );
 };
-exports.updateTour = (req, res) => {
-  res.status(200).json({ status: 'success', results: 1, data: req.body });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true, runValidators: true
+    })
+    res.status(200).json({ status: 'success', results: 1, data: {tour}});
+    
+  } catch (error) {
+    res.status(400).json({status: "Failed", message: error})
+  }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(200).json({ status: 'success', data: null });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id)
+    // const tour = await Tour.findById(req.params.id)
+   
+    res.status(200).json({ status: 'success', data: {message: "Data successfylly deleted"}});
+    
+  } catch (error) {
+    res.status(400).json({status: "Failed", message: error})
+  }
 };
 
 exports.getTour = async (req, res) => {
   try {
    const tour = await Tour.findById(req.params.id)
-   console.log(tour)
+   
   return res.status(200).json({
      status: 'success',
      data: {tour}
--- models/tourModel.js
+++ models/tourModel.js
@@ -2,6 +2,47 @@ const mongoose = require('mongoose');
 const tourSchema = new mongoose.Schema({
     name: {type: String, required: [true, 'A tour must have a name'], unique: true},
     rating: {type: Number, default: 4.5},
+    duration: {
+type: Number,
+required: [true, 'A tour must have a duration']
+    },
+maxGroupSize: {
+  type: Number,
+  required: [true, 'A tour must have a group of ']
+},
+difficulty: {
+  type: Number,
+  required: [true, 'A tour must have a group of ']
+},
+ratingAverage: {
+  type: Number,
+  default: 4.5
+},
+ratingQuantity: {
+  type: Number,
+  default: 0
+},
+
+priceDiscount: Number,
+summary: {
+  type: String,
+  trim: true,
+  required: [true, 'A tour must have a description']
+},
+description: {
+  type: String,
+  trim: true,
+},
+imageCover: {
+  type: String,
+  required: [true, 'A tour must have a cover image']
+},
+images: [String],
+createdAt: {
+  type: Date,
+  default: Date.now()
+},
+startDates: [Date],
     price: {type: Number, required: [true, 'A tour must have a price']}
   })
   
